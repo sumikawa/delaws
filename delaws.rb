df9b556ac7c35f4c7d82f779083dbe33a7c2a544
Munechika Sumikawa <sumikawa@sumikawa.jp>
2014-01-21 17:08:32 +0900

    Ruby way

--- delaws.rb
+++ delaws.rb
@@ -40,19 +40,21 @@ if reservations
   end
 end
 
-elb = Aws::ElasticLoadBalancing.new
-rs = elb.describe_load_balancers.load_balancer_descriptions
-if rs
-  rs.each do |r|
-    findid(r, "(security_groups|subnets|vpc_id)", "load_balancer_name")
+Aws::ElasticLoadBalancing.new do |elb|
+  rs = elb.describe_load_balancers.load_balancer_descriptions
+  if rs
+    rs.each do |r|
+      findid(r, "(security_groups|subnets|vpc_id)", "load_balancer_name")
+    end
   end
 end
 
-rds = Aws::RDS.new
-rs = rds.describe_db_instances.db_instances
-if rs
-  rs.each do |r|
-    findid(r, "(_name|_id)", "db_instance_identifier")
+Aws::RDS.new do |rds|
+  rs = rds.describe_db_instances.db_instances
+  if rs
+    rs.each do |r|
+      findid(r, "(_name|_id)", "db_instance_identifier")
+    end
   end
 end
 
