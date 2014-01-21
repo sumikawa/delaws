2d9299de7b5b2cd749ce912ec9cfde0eba440720
Munechika Sumikawa <sumikawa@sumikawa.jp>
2014-01-21 16:05:53 +0900

    Support expansion of array

--- findid.rb
+++ findid.rb
@@ -1,8 +1,20 @@
 def findid(resource, regex, myid)
   resource.each do |k, v|
-    if k =~ /#{regex}$/ && v != eval("resource.#{myid}")
+    if v.kind_of?(Array)
+      findid_a(v, regex, myid)
+    elsif v.kind_of?(Hash)
+      puts "Hash exists on #{k}"
+    elsif k =~ /#{regex}$/ && v != eval("resource.#{myid}")
       @idx[v] ||= []
       @idx[v].push(eval("resource.#{myid}"))
+    else
+      puts "#{myid}: #{k}: #{v}"
     end
+  end
+end
+
+def findid_a(resource, regex, myid)
+  resource.each do |k|
+    findid(k, regex, myid)
   end
 end
