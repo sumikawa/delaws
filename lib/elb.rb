d98db2301bbfcaa9f73434ae2b4633a96381be4b
Munechika Sumikawa <sumikawa@sumikawa.jp>
2014-01-24 18:35:52 +0900

    s/@elb/@product/

--- lib/elb.rb
+++ lib/elb.rb
@@ -1,11 +1,11 @@
 class DelawsElb < DelawsBase
   def initialize
     @prefix = "elb-"
-    @elb = Aws::ElasticLoadBalancing.new
+    @product = Aws::ElasticLoadBalancing.new
   end
 
   def describe_all
-    rs = @elb.describe_load_balancers.load_balancer_descriptions
+    rs = @product.describe_load_balancers.load_balancer_descriptions
     if rs
       rs.each do |r|
         findid(r, "(security_groups|subnets|vpc_id)", "load_balancer_name", @prefix)
@@ -15,7 +15,7 @@ class DelawsElb < DelawsBase
 
   def describe(name)
     begin
-      @elb.describe_load_balancers(load_balancer_names: [name.gsub(/^#{@prefix}/,"")])
+      @product.describe_load_balancers(load_balancer_names: [name.gsub(/^#{@prefix}/,"")])
       return 0
     rescue
       puts "#{Thread.current.object_id}: not_found #{name}"
@@ -24,7 +24,7 @@ class DelawsElb < DelawsBase
   end
 
   def delete(name)
-    @elb.delete_load_balancer(load_balancer_name: name.gsub(/^#{@prefix}/,""))
+    @product.delete_load_balancer(load_balancer_name: name.gsub(/^#{@prefix}/,""))
     return 0
   end
 end
